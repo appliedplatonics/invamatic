0a1e8a5f1daba1e1c31c94d3d44a0930829cbf21
Josh Myer <josh@appliedplatonics.com>
2012-08-27 19:23:31 -0700

    Cleans up do_import.sh a tiny bit

--- do_import.sh
+++ do_import.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+#
 # This sets up the initial dev environment with good data
+#
+# Before running this, you'll need to create TSV (tab-separated value)
+# files for your line items from the various suppliers. This process
+# needs a great deal more documentation.
+#
+# XXX TODO Document the import flow better.
+#
+
 
 rake db:migrate
 ./script/runner ./script/import_inventory.rb "mouser" ./imported_data/mouser/mouser_import.tsv 
-./script/runner ./script/import_inventory.rb "jameco" ./jameco/jameco_import.tsv 
-./script/runner ./script/import_inventory.rb "tayda" ./tayda/import_tayda.tsv 
+./script/runner ./script/import_inventory.rb "jameco" ./imported_data/jameco/jameco_import.tsv 
 ./script/runner ./script/import_inventory.rb "tayda" ./imported_data/tayda/import_tayda.tsv 
 ./script/runner ./script/import_inventory.rb "pcbcart" ./imported_data/pcbcart/import_pcbcart.tsv 
